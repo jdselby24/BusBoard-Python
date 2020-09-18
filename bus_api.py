5076535c02bdeb1f3ca6f9191888f04b7adf96f2
jselby <joshua.selby@altran.com>
2020-09-18 14:08:31 +0100

    updated api requests to use https

--- bus_api.py
+++ bus_api.py
@@ -14,19 +14,19 @@ class bus_api:
             self.APP_ID = configData['app-id']
 
     def get_bus_info(self,post_code):
-        request = requests.get("http://api.postcodes.io/postcodes/"+post_code.upper())
+        request = requests.get("https://api.postcodes.io/postcodes/"+post_code.upper())
         dict = json.loads(request.text)
         result = dict["result"]
         lat = result["latitude"]
         long = result["longitude"]
 
-        request = requests.get("http://transportapi.com/v3/uk/places.json?lat="+str(lat)+"&lon="+str(long)+"&type=bus_stop",params={'api_key':self.API_KEY,'app_id':self.APP_ID})
+        request = requests.get("https://transportapi.com/v3/uk/places.json?lat="+str(lat)+"&lon="+str(long)+"&type=bus_stop",params={'api_key':self.API_KEY,'app_id':self.APP_ID})
         time.sleep(0.25)
         dict = json.loads(request.text)
         bus_stops = []
         for i in dict["member"][:2]:
             bus_code = i["atcocode"]
-            request = requests.get("http://transportapi.com/v3/uk/bus/stop/"+bus_code+"/live.json",params={'api_key':self.API_KEY,'app_id':self.APP_ID})
+            request = requests.get("https://transportapi.com/v3/uk/bus/stop/"+bus_code+"/live.json",params={'api_key':self.API_KEY,'app_id':self.APP_ID})
             time.sleep(0.25)
             bus_stops.append(request.text)
         return(bus_stops)
